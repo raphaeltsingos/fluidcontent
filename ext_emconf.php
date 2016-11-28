236645d83d5b1eb116bd17aacd9dc799777ed9a4
Claus Due <claus@namelesscoder.net>
2016-11-28 13:15:41 +0100

    [TASK] Update TYPO3 version support constraints (#369)

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.5.0-7.0.99',
-			'typo3' => '7.6.0-8.2.99',
+			'typo3' => '7.6.0-8.4.99',
 			'flux' => '7.3.0-7.99.99',
 		),
 		'conflicts' => array(
