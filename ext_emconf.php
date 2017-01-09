9ec9cdc2aff36b2e436adc04d6a7e45dfb18085c
Marc Neuhaus <marc@mia3.com>
2017-01-09 23:58:48 +0100

    [FEATURE] Raise TYPO3 and Flux version requirement

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,9 +32,9 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '4.4.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.6.0-8.4.99',
-			'flux' => '7.3.0-7.99.99',
       		'php' => '7.0.0-7.1.99',
+			'typo3' => '7.6.0-8.5.99',
+			'flux' => '7.3.0-8.99.99',
 		),
 		'conflicts' => array(
 		),
