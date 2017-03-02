cd19396cc7e5ce976b740697a5698c456a19b6cc
Cedric Ziel <cedric@cedric-ziel.com>
2017-03-02 11:01:44 +0100

    [TASK] Add autoloading information to ext_emconf (#393)

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,23 +19,30 @@ $EM_CONF[$_EXTKEY] = array (
   'clearCacheOnLoad' => 1,
   'lockType' => '',
   'version' => '5.0.0',
-  'constraints' => 
+  'constraints' =>
   array (
-    'depends' => 
+    'depends' =>
     array (
       'php' => '7.0.0-7.1.99',
       'typo3' => '7.6.0-8.6.99',
       'flux' => '7.3.0-8.99.99',
     ),
-    'conflicts' => 
+    'conflicts' =>
     array (
     ),
-    'suggests' => 
+    'suggests' =>
     array (
     ),
   ),
-  'suggests' => 
+  'suggests' =>
   array (
   ),
   '_md5_values_when_last_written' => '',
+  'autoload' =>
+  array(
+      'psr-4' =>
+      array(
+          'FluidTYPO3\\Fluidcontent\\' => 'Classes/',
+      ),
+  ),
 );
