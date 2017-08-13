a2267d159a388e369428a699f7a92cd2d2a1e249
Claus Due <claus@namelesscoder.net>
2017-08-13 13:57:59 +0200

    [REMOVAL] Drop TYPO3 7.6 support
    The minimum requirements are now Flux 8.2 and TYPO3 8.7.

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,8 +24,8 @@ $EM_CONF[$_EXTKEY] = array (
     'depends' => 
     array (
       'php' => '7.0.0-7.1.99',
-      'typo3' => '7.6.0-8.7.99',
-      'flux' => '7.3.0-8.99.99',
+      'typo3' => '8.7.0-8.7.99',
+      'flux' => '8.2.0-9.99.99',
     ),
     'conflicts' => 
     array (
